36ef76044684fa5a9f4b7f99568e76aa59f91740
Philipp Herbst <philipp.herbst@bmw.de>
2025-01-26 19:28:07 +0100

    Changing firebase config´

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ function submitSwitchEstimation() {
 
 function submitStayEstimation() {
   // store result
-  probabilityStay = Number(document.getElementById('probabilityStay').value);
+  probabilitySwitch = Number(document.getElementById('probabilityStayInput').value);
   document.getElementById('probabilitySectionStay').style.display = 'none';
   endGame();
 }
