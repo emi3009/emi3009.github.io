71ee0273a5030341aeedff16ea44abe669ce0532
emi3009 <emilyherbst06@gmail.com>
2025-01-27 08:04:58 +0100

    Update charts.js

--- charts/charts.js
+++ charts/charts.js
@@ -172,5 +172,55 @@ onValue(resultRef, (snapshot) => {
     console.error(error);
 });
 
+/////////////
 
+// Initialize the chart variable
+let stayedProbabilityChart;
+// Get the canvas element
+const canvas4 = document.getElementById('myChart4');
+const ctx4 = canvas3.getContext('2d');
+let myChart4;
+// Retrieve data from the database
+onValue(resultRef, (snapshot) => {
+    const data = snapshot.val();
+    console.log(data);
+
+    // Count the number of stayed and switched
+    const won = Object.values(data)
+        .filter(item => item.didStay)
+        .filter(item => item.carBehind === item.playerChoice).length;
+    const lost = Object.values(data)
+        .filter(item => item.didStay)
+        .filter(item => item.carBehind !== item.playerChoice).length;
+
+    // Chart configuration
+    const chartData = {
+        labels: ['Auto', 'Ziege'],
+        datasets: [{
+            data: [won, lost],
+            backgroundColor: [
+                '#4e73df',
+                '#1cc88a'
+            ],
+            hoverBackgroundColor: [
+                '#2e59d9',
+                '#17a673'
+            ],
+            hoverBorderColor: 'rgba(234, 236, 244, 1)',
+        }]
+    };
+    // If the chart already exists, update it
+    if (myChart4) {
+        myChart4.data = chartData;
+        myChart3.update();
+    } else {
+        // Create the chart if it doesn't exist
+        myChart4 = new Chart(ctx4, {
+            type: 'pie',
+            data: chartData
+        });
+    }
+}, (error) => {
+    console.error(error);
+});
 
